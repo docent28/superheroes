28405ae45bf5d5bc77606b9261924341f29060ed
docent28 <eaa67@mail.ru>
2021-10-09 21:08:01 +0300

    от урока

---
+++ lib/pages/main_page.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:superheroes/blocs/main_bloc.dart';
+import 'package:superheroes/resources/superheroes_colors.dart';
+
+class MainPage extends StatefulWidget {
+  MainPage({Key? key}) : super(key: key);
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  final MainBloc bloc = MainBloc();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: SuperheroesColors.background,
+      body: SafeArea(
+        child: Stack(
+          children: [
+            StreamBuilder<MainPageState>(
+              stream: bloc.observeMainPageState(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData || snapshot.data == null) {
+                  return const SizedBox();
+                }
+                return Center(
+                  child: Text(
+                    snapshot.data!.toString(),
+                    style: TextStyle(color: Colors.white),
+                  ),
+                );
+              },
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: GestureDetector(
+                onTap: () => bloc.nextState(),
+                child: Text(
+                  "Next state".toUpperCase(),
+                  style: TextStyle(fontSize: 20, color: Colors.white),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    bloc.dispose();
+    super.dispose();
+  }
+}
---
+++ lib/resources/superheroes_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/cupertino.dart';
+
+class SuperheroesColors {
+  const SuperheroesColors._();
+
+  static const background = Color(0xFF10141E);
+}

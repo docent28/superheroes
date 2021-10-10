0b62db537cd8b2efa3ee0b2362f7236a88d642b4
docent28 <eaa67@mail.ru>
2021-10-10 21:09:55 +0300

    Прошли первый тесе, выполнили первое задание

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:superheroes/pages/main_page.dart';
 
 void main() {
@@ -12,6 +13,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        textTheme: GoogleFonts.openSansTextTheme(
+          Theme.of(context).textTheme,
+        ),
+      ),
       home: MainPage(),
     );
   }

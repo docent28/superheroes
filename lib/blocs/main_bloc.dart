e15fcb685ed1fd2d1ecdbd9601c185c97a30d212
docent28 <eaa67@mail.ru>
2021-10-23 09:30:15 +0300

    Организовали поиск с возвратом пустого списка

--- lib/blocs/main_bloc.dart
+++ lib/blocs/main_bloc.dart
@@ -3,13 +3,54 @@ import 'dart:async';
 import 'package:rxdart/subjects.dart';
 
 class MainBloc {
-  final BehaviorSubject<MainPageState> stateSubject = BehaviorSubject();
+  static const minSymbols = 3;
 
-  Stream<MainPageState> observeMainPageState() => stateSubject;
+  final BehaviorSubject<MainPageState> stateSubject = BehaviorSubject();
+  final favoritesSuperheroesSubject =
+      BehaviorSubject<List<SuperheroInfo>>.seeded(SuperheroInfo.mocked);
+  final searchedSuperheroesSubject = BehaviorSubject<List<SuperheroInfo>>();
+  final currentTextSubject = BehaviorSubject<String>.seeded("");
+
+  StreamSubscription? textSubscription;
+  StreamSubscription? searchSubscription;
 
   MainBloc() {
     stateSubject.add(MainPageState.noFavorites);
+
+    textSubscription = currentTextSubject.listen((value) {
+      searchSubscription?.cancel();
+      if (value.isEmpty) {
+        stateSubject.add(MainPageState.favorites);
+      } else if (value.length < minSymbols) {
+        stateSubject.add(MainPageState.minSymbols);
+      } else {
+        searchForSuperheroes(value);
+      }
+    });
   }
+
+  void searchForSuperheroes(final String text) {
+    stateSubject.add(MainPageState.loading);
+    searchSubscription = search(text).asStream().listen(
+      (searchResults) {
+        if (searchResults.isEmpty) {
+          stateSubject.add(MainPageState.nothingFound);
+        } else {
+          searchedSuperheroesSubject.add(searchResults);
+          stateSubject.add(MainPageState.searchResults);
+        }
+      },
+      onError: (error, stackTrace) {
+        stateSubject.add(MainPageState.loadingError);
+      },
+    );
+  }
+
+  Future<List<SuperheroInfo>> search(final String text) async {
+    return [];
+  }
+
+  Stream<MainPageState> observeMainPageState() => stateSubject;
 
   void nextState() {
     final currentState = stateSubject.value;
@@ -19,14 +60,20 @@ class MainBloc {
     stateSubject.sink.add(nextState);
   }
 
-  void updateText(final String? text){
-    print("GOT NEW TEXT : $text");
+  void updateText(final String? text) {
+    currentTextSubject.add(text ?? "");
   }
 
   void dispose() {
     stateSubject.close();
+    favoritesSuperheroesSubject.close();
+    searchedSuperheroesSubject.close();
+    currentTextSubject.close();
+
+    textSubscription?.cancel();
   }
 }
+
 //
 enum MainPageState {
   noFavorites,
@@ -39,13 +86,49 @@ enum MainPageState {
 }
 
 class SuperheroInfo {
-
   final String name;
   final String realName;
   final String imageUrl;
 
-  const SuperheroInfo(this.name, this.realName, this.imageUrl);
+  const SuperheroInfo({
+    required this.name,
+    required this.realName,
+    required this.imageUrl,
+  });
 
+  @override
+  String toString() {
+    return 'SuperheroInfo{name: $name, realName: $realName, imageUrl: $imageUrl}';
+  }
 
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is SuperheroInfo &&
+          runtimeType == other.runtimeType &&
+          name == other.name &&
+          realName == other.realName &&
+          imageUrl == other.imageUrl;
 
+  @override
+  int get hashCode => name.hashCode ^ realName.hashCode ^ imageUrl.hashCode;
+
+  static const mocked = [
+    SuperheroInfo(
+      name: "Batman",
+      realName: "Bruce Wayne",
+      imageUrl:
+          "https://www.superherodb.com/pictures2/portraits/10/100/639.jpg",
+    ),
+    SuperheroInfo(
+      name: "Ironman",
+      realName: "Tony Stark",
+      imageUrl: "https://www.superherodb.com/pictures2/portraits/10/100/85.jpg",
+    ),
+    SuperheroInfo(
+      name: "Venom",
+      realName: "Eddie Brock",
+      imageUrl: "https://www.superherodb.com/pictures2/portraits/10/100/22.jpg",
+    ),
+  ];
 }

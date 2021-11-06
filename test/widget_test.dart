b0f6160e7647c50c77024a528e482b2c8bd3dbc3
docent28 <eaa67@mail.ru>
2021-11-06 20:26:16 +0300

    Выполнено 4-е задание

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,7 +4,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'lesson_3/task_1.dart';
 import 'lesson_3/task_2.dart';
 import 'lesson_3/task_3.dart';
-// import 'lesson_3/task_4.dart';
+import 'lesson_3/task_4.dart';
 
 void main() async {
   TestWidgetsFlutterBinding.ensureInitialized();
@@ -12,5 +12,5 @@ void main() async {
   group("l07h01", () => runTestLesson3Task1());
   group("l07h02", () => runTestLesson3Task2());
   group("l07h03", () => runTestLesson3Task3());
-  // group("l07h04", () => runTestLesson3Task4());
+  group("l07h04", () => runTestLesson3Task4());
 }

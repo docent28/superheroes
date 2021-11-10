806cdc94b80d13d2c5287821a5f2d845a3c3d4ed
docent28 <eaa67@mail.ru>
2021-11-10 16:19:17 +0300

    Создали ви разметили хранилище для супергероев

---
+++ lib/favorite_superheroes_storage.dart
@@ -0,0 +1,25 @@
+import 'package:superheroes/model/superhero.dart';
+
+class FavoriteSuperheroesStorage {
+
+  Future<bool> addToFavorites(final Superhero superhero) {
+    throw UnimplementedError();
+  }
+
+  Future<bool> removeFromFavorites(final String id) {
+    throw UnimplementedError();
+  }
+
+  Future<Superhero?> getSuperhero(final String id) {
+    throw UnimplementedError()
+  }
+
+  Stream<List<Superhero>> observeFavoriteSuperheroes(){
+    throw UnimplementedError()
+  }
+
+  Stream<List<Superhero>> observeIsFavorite(final String id){
+    throw UnimplementedError()
+  }
+
+}
